27399171ef5ac8514bc3521788431bc2cbce0150
Admin <admin@Admins-Mac.local>
2018-10-05 14:25:43 +0530

    add map feature to view controller

--- Git tutorial/ViewController.swift
+++ Git tutorial/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("hello world")
+        
+        let mapviewcontroller = MapViewController()
+        print(mapviewcontroller)
     }
 
     override func didReceiveMemoryWarning() {
