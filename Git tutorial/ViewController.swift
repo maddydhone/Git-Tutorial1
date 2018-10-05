3d44f410ab83baa381cba2c75211c515fd85b4c7
Admin <admin@Admins-Mac.local>
2018-10-05 11:55:42 +0530

    Initial Commit

---
+++ Git tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git tutorial
+//
+//  Created by Admin on 05/10/2018.
+//  Copyright © 2018 Avansys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
